e3dc21dc47cfca876cc10c9c7afba8343e57963f
notnishans <nishanmalla51@gmail.com>
2025-08-05 21:40:27 +0545

    Added database connection setup with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
+
+connectDB();
 
 const app = express();
 
